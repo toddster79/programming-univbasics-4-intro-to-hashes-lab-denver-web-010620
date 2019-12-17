b05dd5e0e6dd129d93beefbdd2bd6249eef6fec7
Todd Carlson <todd.carlson3030@me.com>
2019-12-17 23:16:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] = key 
+  if hash[key]
     hash[key] += 1 
+    return hash
   else 
     hash[key] = 1 
   end 
