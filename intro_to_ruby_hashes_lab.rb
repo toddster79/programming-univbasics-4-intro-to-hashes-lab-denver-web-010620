6ca6fb1ae42fa05a855d144c36ee1cacdae8cc4f
Todd Carlson <todd.carlson3030@me.com>
2019-12-17 23:08:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
+  hash = {}
  if hash[key]
-   hash = hash[key] += 1 
+   hash[key] += 1 
  else 
    hash[key] = 1 
  end 
